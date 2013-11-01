4084fa8d73ca6998b69c7539b613609c359f9bcd
Joe Conley <josephpconley@gmail.com>
2013-11-01 13:16:44 -0400

    basic ws tool

--- app/controllers/db/DBTool.scala
+++ app/controllers/db/DBTool.scala
@@ -4,7 +4,7 @@ import play.api._
 import play.api.mvc._
 import models.{SimpleDB}
 
-object Query extends Controller {
+object DBTool extends Controller {
   
   def index = Action { implicit req =>
     //test with swingstats
---
+++ app/controllers/ws/WSTool.scala
@@ -0,0 +1,39 @@
+package controllers.ws
+
+import play.api._
+import play.api.mvc._
+import models.{SimpleDB}
+import play.api.libs.json.Json
+import play.api.libs.ws.WS
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object WSTool extends Controller {
+  
+  def index = Action { implicit req =>
+    Ok(views.html.ws.index())
+  }
+
+  case class Call(url: String, method: String, body:String)
+  case class Response(body: String)
+
+  def call = Action(parse.json) { implicit req =>
+    Json.reads[Call].reads(req.body).fold(
+      invalid => BadRequest,
+      call => {
+        val req = WS.url(call.url)
+
+        Async {
+          val response = call.method match {
+            case "GET" => req.get()
+            case "POST" => req.post(call.body)
+            case _ => throw new RuntimeException("Bad method: " + call.method)
+          }
+
+          response.map{ r =>
+            Ok(Json.writes[Response].writes(Response(r.body)))
+          }
+        }
+      }
+    )
+  }
+}
