f3c0ce08e34d8ca5c3e72121f5f068bdfab65454
Joe Conley <josephpconley@gmail.com>
2013-12-09 17:09:02 -0500

    adding separate modes of unscrambling

--- app/controllers/Anagrams.scala
+++ app/controllers/Anagrams.scala
@@ -21,7 +21,7 @@ object Anagrams extends Controller{
   def solve = Action(parse.json) { implicit req =>
     val bank: Seq[String] = (req.body \ "url").asOpt[String].map{ url =>
       Source.fromURL(url).getLines().toSeq
-    }.getOrElse( Seq((req.body \ "customBank").as[String]))
+    }.getOrElse((req.body \ "customList").as[Seq[String]])
 
     val reads = ((__ \ "input").read[String] and (__ \ "numWild").read[Int]).tupled
     val (input, numWild) = reads.reads(req.body).getOrElse(throw new Exception("Bad JSON"))
