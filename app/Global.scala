b3ff986545b9ad75a73624210b1c99969cc24b61
Joe Conley <josephpconley@gmail.com>
2014-01-16 12:12:52 -0500

    removing h2 db config

--- app/Global.scala
+++ app/Global.scala
@@ -1,5 +1,6 @@
 import play.api._
 
+import play.api.libs.concurrent.Akka
 import play.api.mvc.{Results, RequestHeader}
 
 import play.api.libs.concurrent.Execution.Implicits._
@@ -9,8 +10,13 @@ import play.api.Play.current
 object Global extends GlobalSettings {
 
   override def onStart (app: Application){
-    println("Generating DDL")
+//    println("Generating DDL")
 //    io.SourceFileUtils.writeStringToFile(new java.io.File("slick.sql"),
 //    Models.ddl.dropStatements.mkString("\n", ";\n\n", ";\n") + Models.ddl.createStatements.mkString("\n", ";\n\n", ";\n"));
+
+    //update RSS feeds every 8 hours
+//    Akka.system.scheduler.schedule(0 seconds, 8 hours){
+//
+//    }
   }
 }
