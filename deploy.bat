895e3b1810431c577592db1b906745922db024d4
Joe Conley <josephpconley@gmail.com>
2014-02-03 08:15:17 -0500

    updated deploy script

--- deploy.bat
+++ deploy.bat
@@ -6,4 +6,8 @@ call cd C:\workspace\joec\scala\puzzles
 call sbt package
 call cd C:\workspace\joec
 call cp scala/puzzles/target/scala-2.10/puzzles_2.10-0.1.jar toolbox/lib/puzzles_2.10-0.1.jar
+call cd C:\workspace\joec\toolbox
+call git add .
+call git commit -am %1
 call git push -f heroku HEAD:master
+call git checkout master
