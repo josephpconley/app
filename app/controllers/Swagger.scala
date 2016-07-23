f942761ac45e75b4ceebea4e63fc6496108ec1bf
Joe Conley <josephpconley@gmail.com>
2016-07-22 21:45:17 -0400

    swagger2postman updates

--- app/controllers/Swagger.scala
+++ app/controllers/Swagger.scala
@@ -3,7 +3,6 @@ package controllers
 import java.io.File
 
 import com.josephpconley.swagger2postman.models.swagger.v12
-import com.josephpconley.swagger2postman.models.swagger.v12.Swagger2Postman
 import com.josephpconley.swagger2postman.{CollectionArgs, CollectionFormats}
 import play.api.Logger
 import play.api.libs.json._
@@ -35,20 +34,31 @@ object Swagger
   }
 
   def generate = Action.async(parse.json) { implicit req =>
-    Json.fromJson[CollectionArgs](req.body).fold(
+    Json.fromJson[GenerateRequest](req.body).fold(
       errors => Future.successful {
         Logger.error(JsError.toFlatJson(errors).toString())
         BadRequest
       },
-      args => WS.url(args.docUrl).get().map { res =>
-        val doc = Json.fromJson[v12.SwaggerDoc](res.json).get
-        Ok(Swagger2PostmanPlay.toPostman(doc, args))
+      gen => gen.selectedVersion match {
+        case 1.2 => {
+          val args = CollectionArgs(gen.host, gen.name, gen.headers)
+          WS.url(args.docUrl).get().map { res =>
+            val doc = Json.fromJson[v12.SwaggerDoc](res.json).get
+            Ok(Swagger2PostmanV12.toPostman(doc, args))
+          }
+        }
+        case 2.0 => {
+
+          Ok(Swagger2PostmanV12.toPostman(doc, args))
+        }
       }
     )
   }
 }
 
-object Swagger2PostmanPlay extends Swagger2Postman {
+case class GenerateRequest(host: String, name: String, selectedVersion: Int, swaggerJson: String, headers: Map[String, String] = Map.empty)
+
+object Swagger2PostmanV12 extends v12.Swagger2Postman {
 
   def execute(url: String) = Await.result(WS.url(url).get(), Duration.Inf).body
 }
